14937695fe57eb194d3a1dcac710a90353864514
David Cook <divergentdave@gmail.com>
2010-08-04 20:06:06 -0500

    What was I thinking? Cleaned up campus_alert logic

--- campus_alert/campus_alert.user.js
+++ campus_alert/campus_alert.user.js
@@ -13,9 +13,4 @@
 // @include        http://*.morris.umn.edu/*
 // @include        http://morris.umn.edu/*
 // ==/UserScript==
-domains = ['umn.edu', 'umcrookston.edu'];
-for (var i = 0; i < domains.length; i++) {
-	if (location.hostname.lastIndexOf(domains[i]) == location.hostname.length - domains[i].length) {
-		alert("WARNING\nThis page pertains to a campus other than U of M-TC.");
-	}
-}
+alert("WARNING\nThis page pertains to a campus other than U of M-TC.");
