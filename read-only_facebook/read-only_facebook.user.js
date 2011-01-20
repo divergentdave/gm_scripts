2a69aa6bb894dad3c3f1fd686e3bcc6563dcd411
David Cook <divergentdave@gmail.com>
2011-01-20 14:26:06 -0600

    Another capitalization change

--- read-only_facebook/read-only_facebook.user.js
+++ read-only_facebook/read-only_facebook.user.js
@@ -12,5 +12,5 @@
  * \chrome\userContent.css (or equivalent) and restart your browser.
  */
 
-GM_addStyle("textarea, .inputtext, .uiComposer_Box, .uiComposer, .uiTypeahead, .uiUfiAddComment, .datebox, .uiActionLinks, .GBSearchBox, .commentActions { display: none !important; }\n" +
+GM_addStyle("textarea, .inputtext, .uiComposer_Box, .uiComposer, .uiTypeahead, .uiUfiAddComment, .datebox, .UIActionLinks, .GBSearchBox, .commentActions { display: none !important; }\n" +
 ".gigaboxx_composer>.MessageComposer, .gigaboxx_composer>.MessageComposer textarea, .gigaboxx_composer>.MessageComposer .inputtext, #email, #pass, #password, .uiComposer.MessageComposer { display: block !important; }");
